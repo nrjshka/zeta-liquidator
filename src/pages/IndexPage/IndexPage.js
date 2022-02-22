8c49d6ab585d0a4a60428e067254617d76aac563
Max Korolev <nrjshka@gmail.com>
2022-02-22 20:18:47 +0300

    Added: prettierrc

---
+++ src/pages/IndexPage/IndexPage.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+import { Wallet } from '../../ui'
+
+const IndexPage = () => {
+  return (
+    <Fragment>
+      <header>
+        <Wallet />
+      </header>
+      <main></main>
+    </Fragment>
+  )
+}
+
+export { IndexPage }
